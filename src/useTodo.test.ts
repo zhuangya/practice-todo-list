78649ef374f4ef316be25ba557f52426d8e9a6ee
Ya Zhuang <zhuangya@users.noreply.github.com>
2021-01-22 17:15:49 +0800

    feat: useTodo hook

---
+++ src/useTodo.test.ts
@@ -0,0 +1,124 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useTodo } from "./useTodo";
+
+test("should append todo", () => {
+  const { result } = renderHook(() => useTodo());
+
+  act(() => {
+    result.current.appendTodo("drink");
+  });
+
+  expect(result.current.todoList[0].todo).toBe("drink");
+});
+
+test("should toggle todo", () => {
+  const { result } = renderHook(() => useTodo());
+
+  act(() => {
+    result.current.appendTodo("switch");
+  });
+
+  expect(result.current.todoList[0].todo).toBe("switch");
+  expect(result.current.todoList[0].isCompleted).toBe(false);
+
+  act(() => {
+    result.current.toggleTodo(result.current.todoList[0].id);
+  });
+
+  expect(result.current.todoList[0].isCompleted).toBe(true);
+});
+
+test("should clear all completed", () => {
+  const { result } = renderHook(() =>
+    useTodo([
+      {
+        id: "fake-uuid-1",
+        todo: "eat",
+        isCompleted: true,
+      },
+      {
+        id: "fake-uuid-2",
+        todo: "pray",
+        isCompleted: true,
+      },
+      {
+        id: "fake-uuid-3",
+        todo: "love",
+        isCompleted: true,
+      },
+    ])
+  );
+
+  act(() => {
+    result.current.clearCompleted();
+  });
+
+  expect(result.current.todoList.length).toBe(0);
+});
+
+test("should get correct todolist by filter", () => {
+  const { result } = renderHook(() =>
+    useTodo([
+      {
+        id: "fake-uuid-1",
+        todo: "eat",
+        isCompleted: false,
+      },
+      {
+        id: "fake-uuid-2",
+        todo: "pray",
+        isCompleted: false,
+      },
+      {
+        id: "fake-uuid-3",
+        todo: "love",
+        isCompleted: true,
+      },
+    ])
+  );
+
+  expect(result.current.getTodoListByFilter("all")).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "id": "fake-uuid-1",
+        "isCompleted": false,
+        "todo": "eat",
+      },
+      Object {
+        "id": "fake-uuid-2",
+        "isCompleted": false,
+        "todo": "pray",
+      },
+      Object {
+        "id": "fake-uuid-3",
+        "isCompleted": true,
+        "todo": "love",
+      },
+    ]
+  `);
+
+  expect(result.current.getTodoListByFilter("active")).toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "id": "fake-uuid-1",
+        "isCompleted": false,
+        "todo": "eat",
+      },
+      Object {
+        "id": "fake-uuid-2",
+        "isCompleted": false,
+        "todo": "pray",
+      },
+    ]
+  `);
+  expect(result.current.getTodoListByFilter("completed"))
+    .toMatchInlineSnapshot(`
+    Array [
+      Object {
+        "id": "fake-uuid-3",
+        "isCompleted": true,
+        "todo": "love",
+      },
+    ]
+  `);
+});
---
+++ src/useTodo.ts
@@ -0,0 +1,89 @@
+import { useReducer } from "react";
+import { v4 as uuid } from "uuid";
+
+export const actionsType = {
+  append: "appendTodo",
+  toggle: "toggleTodo",
+  clear: "clearCompleted",
+};
+
+type ITodoItem = {
+  id: string;
+  todo: string;
+  isCompleted: boolean;
+};
+
+type ITodoList = Array<ITodoItem>;
+type IActionLike = { type: string; payload?: any };
+
+function todoReducer(state: ITodoList, action: IActionLike) {
+  switch (action.type) {
+    case actionsType.append: {
+      return [...state, action.payload];
+    }
+    case actionsType.toggle: {
+      return state.map((todo) =>
+        todo.id === action.payload
+          ? {
+              ...todo,
+              isCompleted: !todo.isCompleted,
+            }
+          : todo
+      );
+    }
+    case actionsType.clear: {
+      return state.filter((todo) => !todo.isCompleted);
+    }
+    default:
+      throw new Error("invalid todo action type");
+  }
+}
+
+type IStatus = "all" | "completed" | "active";
+
+const identifyByFilter = (status: IStatus) => (todo: ITodoItem) => {
+  if (status === "all") {
+    return true;
+  } else if (status === "completed") {
+    return todo.isCompleted;
+  } else if (status === "active") {
+    return !todo.isCompleted;
+  } else {
+    throw new Error("invalid todo filter");
+  }
+};
+
+export function useTodo(initialState = [] as ITodoList, reducer = todoReducer) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function appendTodo(todo: string) {
+    dispatch({
+      type: actionsType.append,
+      payload: {
+        id: uuid(),
+        todo,
+        isCompleted: false,
+      },
+    });
+  }
+
+  function toggleTodo(id: string) {
+    dispatch({ type: actionsType.toggle, payload: id });
+  }
+
+  function clearCompleted() {
+    dispatch({ type: actionsType.clear });
+  }
+
+  function getTodoListByFilter(status: IStatus = "all") {
+    return state.filter(identifyByFilter(status));
+  }
+
+  return {
+    todoList: state,
+    appendTodo,
+    toggleTodo,
+    clearCompleted,
+    getTodoListByFilter,
+  };
+}
